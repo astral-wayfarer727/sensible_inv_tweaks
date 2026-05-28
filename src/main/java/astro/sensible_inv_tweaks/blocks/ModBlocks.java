001f8c454eae6d7c257fe6bcd3905b3c03f46027
astral-wayfarer727 <astralwayfarer727@gmail.com>
2026-05-28 16:33:07 +0100

    v1.1.0

--- src/main/java/astro/sensible_inv_tweaks/blocks/ModBlocks.java
+++ src/main/java/astro/sensible_inv_tweaks/blocks/ModBlocks.java
@@ -3,15 +3,17 @@ package astro.sensible_inv_tweaks.blocks;
 import astro.sensible_inv_tweaks.SensibleInventoryTweaks;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.*;
+import net.minecraft.block.enums.NoteBlockInstrument;
 import net.minecraft.item.ItemGroups;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.ColorCode;
 import net.minecraft.util.Identifier;
 import org.spongepowered.include.com.google.common.base.Function;
 
-import static net.minecraft.block.Blocks.IRON_BLOCK;
-import static net.minecraft.block.Blocks.LAPIS_BLOCK;
+import static net.minecraft.block.Blocks.*;
 
 public class ModBlocks {
 
@@ -23,6 +25,14 @@ public class ModBlocks {
         Items.register(block);
         return block;
     }
+
+    //Red Nether Brick Extras
+    public static FenceBlock RED_NETHER_BRICK_FENCE = (FenceBlock) register("red_nether_brick_fence",
+            FenceBlock::new, Block.Settings.copy(RED_NETHER_BRICKS));
+    public static Block RED_CRACKED_NETHER_BRICKS = register("red_cracked_nether_bricks",
+            Block::new, AbstractBlock.Settings.copy(RED_NETHER_BRICKS));
+    public static Block RED_CHISELED_NETHER_BRICKS = register("red_chiseled_nether_bricks",
+            Block::new, AbstractBlock.Settings.copy(RED_NETHER_BRICKS));
 
     //Lapis Blockset
     public static Block LAPIS_STAIRS = register("lapis_stairs",
@@ -51,6 +61,46 @@ public class ModBlocks {
             PillarBlock::new, AbstractBlock.Settings.copy(IRON_BLOCK).luminance(state -> 14));
     public static Block BREEZE_PILLAR = register("breeze_pillar",
             PillarBlock::new, AbstractBlock.Settings.copy(IRON_BLOCK));
+    public static Block FEATHER_BLOCK = register("feather_block",
+            HayBlock::new, AbstractBlock.Settings.copy(HAY_BLOCK).mapColor(MapColor.WHITE).sounds(BlockSoundGroup.WOOL));
+    public static Block GUNPOWDER_BLOCK = register("gunpowder_block",
+            settings -> new ColoredFallingBlock(new ColorCode(-8356741), settings),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.SNARE).strength(0.5F).sounds(BlockSoundGroup.SAND));
+    public static Block SUGAR_BLOCK = register("sugar_block",
+            settings -> new ColoredFallingBlock(new ColorCode(-8356741), settings),
+            AbstractBlock.Settings.create().mapColor(MapColor.WHITE).instrument(NoteBlockInstrument.SNARE).strength(0.5F).sounds(BlockSoundGroup.SAND));
+
+    //Snow and Ice related blocks
+    public static Block PACKED_SNOW = register("packed_snow",
+            Block::new, AbstractBlock.Settings.create().mapColor(MapColor.WHITE).requiresTool().strength(0.5F).sounds(BlockSoundGroup.SNOW));
+    public static Block PACKED_SNOW_STAIRS = register("packed_snow_stairs",
+            settings -> new StairsBlock(PACKED_SNOW.getDefaultState(), settings),
+            Block.Settings.copy(PACKED_SNOW));
+    public static SlabBlock PACKED_SNOW_SLAB = (SlabBlock) register("packed_snow_slab",
+            SlabBlock::new, Block.Settings.copy(PACKED_SNOW));
+    public static WallBlock PACKED_SNOW_WALL = (WallBlock) register("packed_snow_wall",
+            WallBlock::new, Block.Settings.copy(PACKED_SNOW));
+
+    public static Block CUT_PACKED_ICE = register("cut_packed_ice",
+            Block::new, AbstractBlock.Settings.create().mapColor(MapColor.PALE_PURPLE).requiresTool().slipperiness(0.98F).strength(0.5F).sounds(BlockSoundGroup.GLASS));
+    public static Block CUT_PACKED_ICE_STAIRS = register("cut_packed_ice_stairs",
+            settings -> new StairsBlock(CUT_PACKED_ICE.getDefaultState(), settings),
+            Block.Settings.copy(CUT_PACKED_ICE));
+    public static SlabBlock CUT_PACKED_ICE_SLAB = (SlabBlock) register("cut_packed_ice_slab",
+            SlabBlock::new, Block.Settings.copy(CUT_PACKED_ICE));
+    public static WallBlock CUT_PACKED_ICE_WALL = (WallBlock) register("cut_packed_ice_wall",
+            WallBlock::new, Block.Settings.copy(CUT_PACKED_ICE));
+
+
+    public static Block CUT_BLUE_ICE = register("cut_blue_ice",
+            Block::new, AbstractBlock.Settings.create().mapColor(MapColor.PALE_PURPLE).requiresTool().strength(2.8F).slipperiness(0.989F).sounds(BlockSoundGroup.GLASS));
+    public static Block CUT_BLUE_ICE_STAIRS = register("cut_blue_ice_stairs",
+            settings -> new StairsBlock(CUT_BLUE_ICE.getDefaultState(), settings),
+            Block.Settings.copy(CUT_BLUE_ICE));
+    public static SlabBlock CUT_BLUE_ICE_SLAB = (SlabBlock) register("cut_blue_ice_slab",
+            SlabBlock::new, Block.Settings.copy(CUT_BLUE_ICE));
+    public static WallBlock CUT_BLUE_ICE_WALL = (WallBlock) register("cut_blue_ice_wall",
+            WallBlock::new, Block.Settings.copy(CUT_BLUE_ICE));
 
     public static void registerBlocks() {
         SensibleInventoryTweaks.LOGGER.info("Registering Blocks for " + SensibleInventoryTweaks.MOD_ID);
@@ -64,8 +114,31 @@ public class ModBlocks {
             entries.add(SMOOTH_LAPIS_STAIRS);
             entries.add(SMOOTH_LAPIS_SLAB);
             entries.add(LAPIS_PILLAR);
+
             entries.add(BLAZE_PILLAR);
             entries.add(BREEZE_PILLAR);
+            entries.add(FEATHER_BLOCK);
+            entries.add(GUNPOWDER_BLOCK);
+            entries.add(SUGAR_BLOCK);
+
+            entries.add(RED_NETHER_BRICK_FENCE);
+            entries.add(RED_CHISELED_NETHER_BRICKS);
+            entries.add(RED_CRACKED_NETHER_BRICKS);
+
+            entries.add(PACKED_SNOW);
+            entries.add(PACKED_SNOW_STAIRS);
+            entries.add(PACKED_SNOW_SLAB);
+            entries.add(PACKED_SNOW_WALL);
+
+            entries.add(CUT_PACKED_ICE);
+            entries.add(CUT_PACKED_ICE_STAIRS);
+            entries.add(CUT_PACKED_ICE_SLAB);
+            entries.add(CUT_PACKED_ICE_WALL);
+
+            entries.add(CUT_BLUE_ICE);
+            entries.add(CUT_BLUE_ICE_STAIRS);
+            entries.add(CUT_BLUE_ICE_SLAB);
+            entries.add(CUT_BLUE_ICE_WALL);
         });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
             entries.add(BLAZE_PILLAR);
--- src/main/java/astro/sensible_inv_tweaks/datagen/SensibleInventoryModelProvider.java
+++ src/main/java/astro/sensible_inv_tweaks/datagen/SensibleInventoryModelProvider.java
@@ -16,15 +16,43 @@ public class SensibleInventoryModelProvider extends FabricModelProvider {
 
     @Override
     public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        //Lapis Blockset
         BlockStateModelGenerator.BlockTexturePool lapisBlockExtrasPool = blockStateModelGenerator.registerCubeAllModelTexturePool(Blocks.LAPIS_BLOCK);
         lapisBlockExtrasPool.stairs(ModBlocks.LAPIS_STAIRS);
         lapisBlockExtrasPool.slab(ModBlocks.LAPIS_SLAB);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.CHISELED_LAPIS);
         blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.LAPIS_BRICKS);
 
+        //Compression Blocks
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.GUNPOWDER_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.SUGAR_BLOCK);
+
+        //Red Nether Brick Extras
+        BlockStateModelGenerator.BlockTexturePool redNetherBrickPool = blockStateModelGenerator.registerCubeAllModelTexturePool(Blocks.RED_NETHER_BRICKS);
+        redNetherBrickPool.fence(ModBlocks.RED_NETHER_BRICK_FENCE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RED_CHISELED_NETHER_BRICKS);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RED_CRACKED_NETHER_BRICKS);
+
+        //Smooth Lapis
         BlockStateModelGenerator.BlockTexturePool smoothLapisPool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.SMOOTH_LAPIS);
         smoothLapisPool.stairs(ModBlocks.SMOOTH_LAPIS_STAIRS);
         smoothLapisPool.slab(ModBlocks.SMOOTH_LAPIS_SLAB);
+
+        //Snow and Ice related blocks
+        BlockStateModelGenerator.BlockTexturePool packedSnowPool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.PACKED_SNOW);
+        packedSnowPool.stairs(ModBlocks.PACKED_SNOW_STAIRS);
+        packedSnowPool.slab(ModBlocks.PACKED_SNOW_SLAB);
+        packedSnowPool.wall(ModBlocks.PACKED_SNOW_WALL);
+
+        BlockStateModelGenerator.BlockTexturePool packedIcePool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.CUT_PACKED_ICE);
+        packedIcePool.stairs(ModBlocks.CUT_PACKED_ICE_STAIRS);
+        packedIcePool.slab(ModBlocks.CUT_PACKED_ICE_SLAB);
+        packedIcePool.wall(ModBlocks.CUT_PACKED_ICE_WALL);
+
+        BlockStateModelGenerator.BlockTexturePool blueIcePool = blockStateModelGenerator.registerCubeAllModelTexturePool(ModBlocks.CUT_BLUE_ICE);
+        blueIcePool.stairs(ModBlocks.CUT_BLUE_ICE_STAIRS);
+        blueIcePool.slab(ModBlocks.CUT_BLUE_ICE_SLAB);
+        blueIcePool.wall(ModBlocks.CUT_BLUE_ICE_WALL);
     }
 
     @Override
